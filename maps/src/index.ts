72abdbafc27f1dc34a0e24643eae863dff8e2d45
Emily Harber <emily.a.harber@gmail.com>
2020-08-14 21:29:57 -0400

    the map exists! 🎉

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Sup Bitches!?');
+// import { User } from './User';
+// import { Company } from './Company';
+
+new google.maps.Map(document.getElementById('map') as Element, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
