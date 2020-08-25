fda6ec943a3028c638cae1836813c7c3fb4a5d1c
Emily Harber <emily.a.harber@gmail.com>
2020-08-25 15:17:48 -0400

    working on stats project with csv file reading and data analysis

---
+++ stats/build/index.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var CsvFileReader_1 = require("./CsvFileReader");
+var reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+console.log(reader.data);
+// enum - enumeration/enumerable - a collection of closely related values
+var MatchResults;
+(function (MatchResults) {
+    MatchResults["HomeWin"] = "H";
+    MatchResults["AwayWin"] = "A";
+    MatchResults["Draw"] = "D";
+})(MatchResults || (MatchResults = {}));
+var manUnitedWins = 0;
+for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
+    var match = _a[_i];
+    if (match[1] === 'Man United' && match[5] === MatchResults.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResults.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log("Manchester United won " + manUnitedWins + " games.");
---
+++ stats/src/index.ts
@@ -0,0 +1,25 @@
+import { CsvFileReader } from './CsvFileReader';
+
+const reader = new CsvFileReader('football.csv');
+reader.read();
+
+console.log(reader.data);
+
+// enum - enumeration/enumerable - a collection of closely related values
+enum MatchResults {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+let manUnitedWins = 0;
+
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResults.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResults.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Manchester United won ${manUnitedWins} games.`);
